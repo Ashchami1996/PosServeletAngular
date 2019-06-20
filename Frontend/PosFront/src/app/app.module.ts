3f928bdd5fadc65f08d41de7507c53a20a977781
AshChami1996 <ashanchamika299@gmail.com>
2019-06-21 00:39:31 +0530

    commit with context.xml

--- Frontend/PosFront/src/app/app.module.ts
+++ Frontend/PosFront/src/app/app.module.ts
@@ -3,14 +3,42 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ManageCustomerComponent } from './view/manage-customer/manage-customer.component';
+import { ItemsComponent } from './view/items/items.component';
+import { OrderComponent } from './view/order/order.component';
+import { MainComponent } from './view/main/main.component';
+import {RouterModule, Routes} from "@angular/router";
+
+const  routes : Routes =[
+  {
+    path : 'Customer',
+    component: ManageCustomerComponent
+  },
+  {
+    path : 'item',
+    component: ItemsComponent
+  },
+  {
+    path: 'order',
+    component: OrderComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ManageCustomerComponent,
+    ItemsComponent,
+    OrderComponent,
+    MainComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
