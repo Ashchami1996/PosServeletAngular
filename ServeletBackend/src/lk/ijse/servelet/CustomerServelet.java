66bb4edf8982af3d466c9e6c2759051c93feeac0
AshChami1996 <ashanchamika299@gmail.com>
2019-06-21 12:57:15 +0530

    cus add

--- ServeletBackend/src/lk/ijse/servelet/CustomerServelet.java
+++ ServeletBackend/src/lk/ijse/servelet/CustomerServelet.java
@@ -1,10 +1,7 @@
 package lk.ijse.servelet;
 
 import javax.annotation.Resource;
-import javax.json.Json;
-import javax.json.JsonArray;
-import javax.json.JsonArrayBuilder;
-import javax.json.JsonObjectBuilder;
+import javax.json.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,9 +9,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.io.PrintWriter;
+import java.sql.*;
 
 @WebServlet(urlPatterns = "/customers")
 public class CustomerServelet extends HttpServlet {
@@ -24,30 +20,126 @@ public class CustomerServelet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try {
+        try (PrintWriter out = resp.getWriter()) {
+            System.out.println("search" + req.getParameter("id"));
+            if (req.getParameter("id") != null) {
+                resp.setContentType("application/json");
+                System.out.println("search");
+                String id = req.getParameter("id");
+
+                try {
+                    Connection connection = ds.getConnection();
+
+                    PreparedStatement pstm = connection.prepareStatement("SELECT * FROM Customer WHERE cid=?");
+                    pstm.setObject(1, id);
+                    ResultSet rst = pstm.executeQuery();
+
+                    if (rst.next()) {
+                        JsonObjectBuilder ob = Json.createObjectBuilder();
+                        ob.add("id", rst.getString(1));
+                        ob.add("name", rst.getString(2));
+                        ob.add("address", rst.getString(3));
 
 
-            Connection connection = ds.getConnection();
-            Statement stm = connection.createStatement();
-            ResultSet rst = stm.executeQuery("SELECT * FROM Customer");
+                        resp.setContentType("application/json");
+                        out.println(ob.build());
+                    } else {
+                        resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+                    }
 
-            JsonArrayBuilder ab = Json.createArrayBuilder();
-            while (rst.next()) {
-                JsonObjectBuilder ob = Json.createObjectBuilder();
-                ob.add("id", rst.getString("id"));
-                ob.add("name", rst.getString("name"));
-                ob.add("address", rst.getString("address"));
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
 
-                ab.add(ob.build());
+
+//                                      customer getall
+
+            } else {
+                try {
+                    System.out.println("get All");
+                    Connection connection = ds.getConnection();
+
+                    Statement stm = connection.createStatement();
+                    ResultSet rst = stm.executeQuery("SELECT * FROM Customer");
+
+                    JsonArrayBuilder customers = Json.createArrayBuilder();
+
+                    while (rst.next()) {
+                        String cid = rst.getString("id");
+                        String name = rst.getString("name");
+                        String address = rst.getString("address");
+
+
+                        JsonObject customer = Json.createObjectBuilder()
+                                .add("id", cid)
+                                .add("name", name)
+                                .add("address", address)
+
+                                .build();
+                        customers.add(customer);
+                    }
+
+                    ((PrintWriter) out).println(customers.build().toString());
+
+                    connection.close();
+                } catch (Exception ex) {
+                    resp.sendError(500, ex.getMessage());
+                    ex.printStackTrace();
+                }
             }
-            JsonArray customers = ab.build();
-            resp.setContentType("application/json");
-            resp.getWriter().println(customers);
-
-            connection.close();
-        } catch (Exception e) {
-            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            e.printStackTrace();
         }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        JsonReader reader = Json.createReader(req.getReader());
+        resp.setContentType("application/json");
+
+        PrintWriter out = resp.getWriter();
+
+        Connection connection = null;
+
+        try {
+            JsonObject customer = reader.readObject();
+            String id = customer.getString("id");
+            String name = customer.getString("name");
+            String address = customer.getString("address");
+            connection = ds.getConnection();
+            System.out.println(id);
+            PreparedStatement pstm = connection.prepareStatement("INSERT INTO Customer VALUES (?,?,?)");
+            pstm.setObject(1, id);
+            pstm.setObject(2, name);
+            pstm.setObject(3, address);
+            boolean result = pstm.executeUpdate()>0;
+
+            if (result){
+                out.println("true");
+            }else{
+                out.println("false");
+            }
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+            out.println("false");
+        }finally {
+            try {
+                connection.close();
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+            out.close();
+        }
+
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
     }
 }
