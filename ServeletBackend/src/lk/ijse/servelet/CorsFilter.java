f6a40745640229f1a5bd0e5aa9c0cd5712027fef
AshChami1996 <ashanchamika299@gmail.com>
2019-06-21 13:57:55 +0530

    corseFilter

---
+++ ServeletBackend/src/lk/ijse/servelet/CorsFilter.java
@@ -0,0 +1,25 @@
+package lk.ijse.servelet;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/*")
+public class CorsFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        response.setHeader("Access-Control-Allow-Origin","http://localhost:4200");
+        response.setHeader("Access-Control-Allow-Methods","OPTIONS, GET, POST, PUT, DELETE");
+        response.setHeader("Access-Control-Allow-Headers","Content-Type");
+
+        super.doFilter(request, response, chain);
+    }
+}
